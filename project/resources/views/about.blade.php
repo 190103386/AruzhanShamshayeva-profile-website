d67dc28977e0b54eb93610eb1d82606361b5fdf9
Aruzhan Shamshayeva <190103386@stu.sdu.edu.kz>
2021-02-22 21:51:01 +0500

    second commit

---
+++ project/resources/views/about.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Patsy Doherty - About</title>
+
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Boostrap Core CSS-->
+        <link rel="stylesheet" href="css/bootstrap.min.css"> 
+        
+        <!-- Main CSS -->
+        <link rel="stylesheet" href="css/style.css">
+        
+        
+        
+        <!-- Google fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Qwigley" >
+        <!-- Font awesome -->
+        <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+   </head>
+   <body>
+   
+   <!-- Start wrapper -->
+   <div class="wrapper">
+  	  <div class="col-md-12">
+         <!-- Logo -->
+         <div class="brand wow fadeIn" data-wow-delay="0.1s"> Shamshayeva Aruzhan
+           <div class="title"> - WELCOME !!! -  </div>
+         </div>
+
+         <!-- Navigation -->
+         <nav class="navbar navbar-default" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <div class="navbar-brand"><a href="#">Patsy Doherty</a>
+                <div class="title"> - hat designer - </div>
+                </div>
+                 
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="index.html" >Home</a>
+                    </li>
+                    <li class="active">
+                       <a href="{{route('about')}}">About</a>
+                    </li>
+                    <li>
+                       <a href="{{route('contact')}}">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /navbar-collapse -->
+         </nav>
+         <!-- End nav -->
+
+
+         <!-- Start margin -->
+         <div class="margin-about">
+             <!-- Start left column -->
+             <div class="box-about col-md-4 col-xs-offset-1">
+               <h3 class="heading-inner">About me</h3>
+                  <div class="hr"></div>
+                    <h4>
+                    <span class="text-about">Let me introduce myself.</span>  My name is Shamshayeva Aruzhan. I was born on the 22th of February 2002 in Moscow. I’m nineteen years old. I’m usually emotional and active person. My life is very diverse. 
+                    </h4>
+                    
+                    <h4>
+                    <span class="text-about"> I think that I have a charitable nature, because I always help people.</span>  I’m usually calm, but sometimes I can lose my temper and become either angry or sad. I like to laugh and joke. I have got a sense of humor.
+                    </h4>
+                    
+                    <h4>
+                    <span class="text-about">I’m hard-working and ambitious person.</span> I set high goals for myself and I work hard to reach them. I’m sociable, so I have got a lot of friends. I appreciate people’s honesty, frankness and politeness. I don’t like when people are rude and aggressive. if you're willing to be jaunty, to set them at an angle and to walk beneath them with a spring in your stride as if you're only a step away from dancing. They demand a lot of you.</h4>
+                    <h4>         
+                   
+             </div>
+             <!-- End left column -->
+             
+             <!-- Start right image -->
+             <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
+               <img src="C:/Users/Админ/Downloads/imgonline-com-ua-Black-White-YyUgGFtH306j22hn.jpg" alt="" class="photo-about" " /> 
+             </div>
+             <!-- End right image -->
+         </div>
+         <!-- End margin -->
+         
+         <div class="col-md-12">
+         </div>
+         
+         
+         <!-- Start margin -->
+         <div class="margin-about">
+         <!-- Start right image -->
+             <div class="col-md-6 wow fadeIn col-md-offset-1" data-wow-delay="0.1s">
+               <img src="C:/xampp/htdocs/images.jfif" alt="" class="photo-about" width="600px" height="900px" /> 
+             </div>
+             <!-- End right image -->
+             <!-- Start left column -->
+             <div class="box-about col-md-4">
+               <h3 class="heading-inner">My Hobby</h3>
+                  <div class="hr"></div>
+                    <div class="media">
+                   <div class="pull-left">
+                       <span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fa fa-star fa-stack-1x fa-inverse"></i>
+                       </span> 
+                   </div>
+                   <div class="media-body">
+                       <h4 class="media-heading">Hobby 1</h4>
+                       <p>Hobbies make our life more colouful and they keep us occupied in our leisure time. And I believe hobbies help us both to relax and to develop. I learnt to imitate drawing nature, beautiful buildings, human faces and bodies, animals and birds. I practiced in making them realistic and natural. My hobby made me observe the world around me every minute of my life.. </p>
+                   </div>
+               </div>
+               <div class="media">
+                   <div class="pull-left">
+                       <span class="fa-stack fa-2x">
+                            <i class="fa fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fa fa-lightbulb-o fa-stack-1x fa-inverse"></i>
+                       </span> 
+                   </div>
+                   <div class="media-body">
+                       <h4 class="media-heading">Hobby 2</h4>
+                       <p> I like reading, meeting new people. But my real passion is singing.I don’t know if I have any talent. All my relatives say that I have beautiful and strong voice. Like any other people I like music, but singing plays a special role. I sing the songs at home, at school. But I never thought to become a popular singer or to make career. I just want to sing and enjoy the process.</p>
+                   </div>
+               </div>
+              
+                               
+             </div>
+             <!-- End left column -->
+             
+             
+         </div>
+      </div>
+      <!-- End col-md-12 -->
+
+   </div>
+   <!-- End wrapper -->
+
+      
+   <!-- jQuery Version 1.11.0 -->
+   <script src="js/jquery-1.11.0.js"></script>
+   <!-- Boostrap JS -->
+   <script src="js/bootstrap.min.js"></script>
+   <!-- Smooth scroll JS -->
+   <script src="js/smoothscroll.js"></script>
+   
+   <!-- Wow JavaScript -->
+   <script src="js/wow.js"></script>
+    
+    <script>
+    new WOW().init();
+    </script>
+
+
+</body>
+</html>
