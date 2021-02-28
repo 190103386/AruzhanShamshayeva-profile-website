13c04a7623c273983e544a8065be1dc5654d5fe1
Aruzhan Shamshayeva <190103386@stu.sdu.edu.kz>
2021-02-28 18:32:44 +0500

    New routes

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Post;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/post/create', function () {
+   DB::table('post')->insert([
+   'title'=>'Save Earth.',
+   'body'=>'Earth and the resources of earth make life possible on it. If we were to imagine our lives without these resources, that would not be possible. '
+   ]);
+});
+Route::get('/post', function () {
+   $post= Post::find(1);
+   return $post->body;
+});
+
